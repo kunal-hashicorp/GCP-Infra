5d229d1459a8b92f7d7691699f0f955f209615e5
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-06-29 22:33:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "gcp_project" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "instance_type" {
+  description = "GCE machine type"
+  type        = string
+  default     = "e2-micro"
+}
