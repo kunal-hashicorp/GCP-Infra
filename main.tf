7d163dcb2975101215cacb7d2d51411fda54756a
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-06-29 23:56:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# main.tf
-
 provider "google" {
   project = var.gcp_project
   region  = var.gcp_region
@@ -29,7 +27,7 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     network       = "default"
-    access_config {}
+    access_config {}  # Enables external IP for SSH
   }
 
   service_account {
@@ -37,9 +35,14 @@ resource "google_compute_instance" "vm_instance" {
     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
+  metadata = {
+    ssh-keys = "jenkins:${file("${path.module}/keys/jenkins_gcp_key.pub")}"
+  }
+
   tags = ["terraform-vm"]
 }
 
 output "instance_public_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  description = "Public IP of the created VM"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
