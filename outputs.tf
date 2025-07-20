39276e1fd7cd6fa0c59cf3fbf0c9242e0d63c98c
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-07-20 15:34:30 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "instance_public_ip" {
+  description = "The public IP address of the VM"
+  value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
+
+output "instance_private_ip" {
+  description = "The private IP address of the VM"
+  value       = google_compute_instance.vm_instance.network_interface[0].network_ip
+}
+
+output "dns_full_record" {
+  value = "${var.dns_record_name}.kunalsingh-17b577.gcp.sbx.hashicorpdemo.com."
+}
+
