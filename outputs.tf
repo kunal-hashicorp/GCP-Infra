78d82904950268ecf5d901d2c8f03b362d629a7d
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-07-24 22:33:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+# outputs.tf
+
 output "instance_public_ip" {
   description = "The public IP address of the VM"
   value       = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
@@ -9,5 +11,14 @@ output "instance_private_ip" {
 }
 
 output "dns_full_record" {
-  value = trimsuffix("${var.dns_record_name}.kunalsingh-17b577.gcp.sbx.hashicorpdemo.com.", ".")
+  value = trimsuffix("${var.dns_record_name}.${var.dns_domain}.", ".")
+}
+
+output "postgres_endpoint" {
+  description = "PostgreSQL instance connection endpoint"
+  value       = google_sql_database_instance.postgres_instance.connection_name
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.vm_bucket.name
 }
