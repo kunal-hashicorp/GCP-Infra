7b050993b9f798ff8a265e92a68e907a78ef4273
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-08-27 15:46:08 +0530

    Update dns

--- outputs.tf
+++ outputs.tf
@@ -9,5 +9,5 @@ output "instance_private_ip" {
 }
 
 output "dns_full_record" {
-  value = trimsuffix("${var.dns_record_name}.${google_dns_managed_zone.dns_zone.dns_name}", ".")
+  value = trimsuffix("${var.dns_record_name}.${var.dns_domain}", ".")
 }
