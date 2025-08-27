9ed7fd40234a2ca6400f06c0da2fd1459d2291da
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-08-27 15:16:58 +0530

    Update dns name

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "a_record" {
-  name         = "${var.dns_record_name}.kunalsingh-9cdbcc.gcp.sbx.hashicorpdemo.com."
+  name         = "${var.dns_record_name}.${var.dns_domain}."
   type         = "A"
   ttl          = var.dns_record_ttl
   managed_zone = "doormat-useremail"
