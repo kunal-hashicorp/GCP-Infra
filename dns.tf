fcb87df4a56587fd49bb93d1e1d3fc966a70fe74
kunal-hashicorp <kunal.singh@hashicorp.com>
2025-09-13 10:14:51 +0530

    Update dns.tf

--- dns.tf
+++ dns.tf
@@ -1,7 +1,27 @@
+variable "dns_record_name" {
+  type        = string
+  description = "Subdomain for the DNS record (leave empty for root record)"
+  default     = ""
+}
+
+variable "dns_record_ttl" {
+  type        = number
+  description = "TTL for the DNS record"
+  default     = 300
+}
+
 resource "google_dns_record_set" "a_record" {
-  name         = "${var.dns_record_name}.${var.dns_domain}."
+  # Build record name safely, strip extra dots
+  name = trim(
+    "${var.dns_record_name != "" ? "${var.dns_record_name}." : ""}kunalsingh-ee7c5e.gcp.sbx.hashicorpdemo.com",
+    "."
+  )
+
   type         = "A"
   ttl          = var.dns_record_ttl
   managed_zone = "doormat-useremail"
-  rrdatas      = [google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip]
+
+  rrdatas = [
+    google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  ]
 }
